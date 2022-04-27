a9f9aab6e004bcc557e53d8db463b7d437ad9de9
Daisuke-993 <dushimaru@bizmates.jp>
2022-04-27 12:30:28 +0900

    feat: add gqlgen #3

--- src/main.go
+++ src/main.go
@@ -4,12 +4,36 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"net/http"
+
+	"main/graph"
+	"main/graph/generated"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 )
 
 var e = createMux()
 
 func main() {
 	e.GET("/", articleIndex)
+
+	// 追記ここから
+	graphqlHandler := handler.NewDefaultServer(
+		generated.NewExecutableSchema(
+			generated.Config{Resolvers: &graph.Resolver{}},
+		),
+	)
+	playgroundHandler := playground.Handler("GraphQL", "/query")
+
+	e.POST("/query", func(c echo.Context) error {
+		graphqlHandler.ServeHTTP(c.Response(), c.Request())
+		return nil
+	})
+
+	e.GET("/playground", func(c echo.Context) error {
+		playgroundHandler.ServeHTTP(c.Response(), c.Request())
+		return nil
+	})
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
